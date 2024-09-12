6479f47a92c1e626e98e0544a8d55ef64130a66f
Landrystar <ngankamlandry88@gmail.com>
2024-09-12 15:17:16 -0400

    new

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" {
+resource "aws_instance" "example" {
   ami           = "ami-032346ab877c418af"
   instance_type = "t3.large"
 }
